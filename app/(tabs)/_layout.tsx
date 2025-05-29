29022c4ea88df1921fb9c5aab65496043ee90928
CaduOMenezes <dudumneto@hotmail.com>
2025-05-29 10:05:04 +0100

    UnitSettingsProvider

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,17 @@
 import { Tabs } from 'expo-router';
+import { UnitSettingsProvider } from '../../utils/UnitSettingsContext'; // adjust path as needed
+
 
 export default function TabLayout() {
   return (
+    <UnitSettingsProvider>
+
     <Tabs>
       <Tabs.Screen name="(home)" />
       <Tabs.Screen name="settings" />
+      <Tabs.Screen name = "search"/>
     </Tabs>
+
+    </UnitSettingsProvider>
   );
 }
